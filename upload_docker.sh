f28a51554a1f39816bfd7a0371f56851ea911b09
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-09-06 18:03:27 +0100

    Complete Docker upload script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="devopshiker42/boston-housing:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag boston-housing $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
+
