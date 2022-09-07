8db2c4d8a5a30dd7b093a99692f80b5e0fb50437
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-09-07 16:32:16 +0100

    [ci skip] complete kubernetes run script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="devopshiker42/boston-housing:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run boston-housing --image=$dockerpath --port=80 --labels app=boston-housing
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward boston-housing 8000:80
 
