4d2ee7560b1044bdcec070eac7d099a229067066
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-09-08 16:35:56 +0100

    [ci skip] update kubernetes run script for minikube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,11 +3,12 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
+# Set Docker ID/path
 dockerpath="devopshiker42/boston-housing:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+minikube image load $dockerpath
 kubectl run boston-housing --image=$dockerpath --port=80 --labels app=boston-housing
 
 # Step 3:
@@ -15,6 +16,7 @@ kubectl run boston-housing --image=$dockerpath --port=80 --labels app=boston-hou
 kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
+# Forward the container port to host once pod ready
+kubectl wait --for=condition=ready pod -l app=boston-housing
 kubectl port-forward boston-housing 8000:80
 
